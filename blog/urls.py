50cbfaf1f13bd7ff8771b79842dad6e403d793fd
Kik <tina.miklasova@gmail.com>
2016-10-27 17:13:59 +0200

    Post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
 from . import views
 from django.conf.urls import url 
 
-urlpatterns = [ url(r'^$', views.post_list, name='post_list')]
+urlpatterns = [
+        url(r'^$', views.post_list, name='post_list'),
+        url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+]
