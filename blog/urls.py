01c0ad72e62053e0e33c05168f804ee247d79711
Kik <tina.miklasova@gmail.com>
2016-10-27 17:54:49 +0200

    post_new method

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 urlpatterns = [
         url(r'^$', views.post_list, name='post_list'),
         url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
 ]
