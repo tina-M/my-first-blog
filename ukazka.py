80543731f360a04f5994f08460a7459efae0f9eb
Kik <tina.miklasova@gmail.com>
2016-10-27 12:12:14 +0200

    Prvá revízia.

---
+++ ukazka.py
@@ -0,0 +1,19 @@
+print(1 + 2)
+
+def hi(name):
+    """Toto je dokumentacny komentar / retazec"""  
+    print('Hi ' + name +'!')
+    print('How are you?')
+
+#hi('Eli')
+
+# girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+# for name in girls:
+#     hi(name)
+
+d = {1:2, 3:4}
+for key, value in d.items():
+    print(key, value)
+
+for i in range(10):
+    print(i)
